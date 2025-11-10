ca41523c9dce02b285ddd9a6b6c4967616eb50ad
Mike-Manzano <61585583+Mike-Manzano@users.noreply.github.com>
2025-11-10 17:04:57 +0100

    Add files via upload

---
+++ Proyecto_Test_Github/src/main/java/com/mycompany/proyecto_test_github/Proyecto_Test_Github.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_test_github;
+
+/**
+ *
+ * @author WEB1-34
+ */
+public class Proyecto_Test_Github {
+
+    public static void main(String[] args) {
+        System.out.println("Este proyecto es un proyecto de prueba para investigar cosillas con gitHub");
+        System.out.println("Hola Gabri, Hola Marius");
+    }
+}
